f2bb245f00797642150e67d0fd2c34cd16ec29ca
alwong18 <alwong18@s.sfusd.edu>
2024-03-01 14:28:06 -0800

    buttons work

--- Scripts/start_menu.gd
+++ Scripts/start_menu.gd
@@ -1,11 +1,12 @@
 extends CenterContainer
 
+@onready var start_game_button = %StartGameButton
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	start_game_button.grab_focus()
 
+func _on_start_game_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/World.tscn")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_quit_button_pressed():
+	get_tree().quit()
