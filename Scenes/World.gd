e54c1e7e3aaee6055a5c4ac05692bd7baa3b75d7
alwong18 <alwong18@s.sfusd.edu>
2024-03-05 12:42:20 -0800

    added fading
    need to fix later, looks weird

---
+++ Scenes/World.gd
@@ -0,0 +1,7 @@
+extends Node
+
+@onready var collision_shape_2d = $StaticBody2D/CollisionShape2D
+@onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
+
+func _ready():
+	pass
--- Scripts/start_menu.gd
+++ Scripts/start_menu.gd
@@ -3,10 +3,13 @@ extends CenterContainer
 @onready var start_game_button = %StartGameButton
 
 func _ready():
+#	RenderingServer.set_default_clear_color(Color.BLACK)
 	start_game_button.grab_focus()
 
 func _on_start_game_button_pressed():
+	await LevelTransition.fade_from_black()
 	get_tree().change_scene_to_file("res://Scenes/World.tscn")
+#	LevelTransition.fade_to_black()
 
 func _on_quit_button_pressed():
 	get_tree().quit()
