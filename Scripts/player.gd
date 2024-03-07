7800a0f2a1c0e0455e1e2fceae324145c3572f8b
machen18 <machen18@s.sfusd.edu>
2024-03-07 14:31:07 -0800

    gravity and friction functions

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,28 +1,38 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 100.0
+const ACCELERATION = 800.0
+const FRICTION = 1000.0
+const JUMP_VELOCITY = -300.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@onready var coyote_jump_timer = $CoyoteJumpTimer
 
 func _physics_process(delta):
-	# Add the gravity.
+	apply_gravity(delta)
+	handle_jump()
+	var input_axis = Input.get_axis("ui_left", "ui_right")
+	handle_acceleration(input_axis, delta)
+	apply_friction(input_axis, delta)
+	move_and_slide()
+
+func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+func handle_jump():
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
+		else:
+			if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
+				velocity.y = JUMP_VELOCITY / 2
 
-	# Handle jump.
-	if Input.is_action_just_pressed("move_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func handle_acceleration(input_axis, delta):
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+func apply_friction(input_axis, delta):
+		if input_axis == 0:
+			velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
