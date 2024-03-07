203f35b29025a3aa250d90176bb82dd05bfbb559
alwong18 <alwong18@s.sfusd.edu>
2024-03-07 14:10:26 -0800

    added the background
    Change later to fit the scenes, testing for now

--- Scripts/start_menu.gd
+++ Scripts/start_menu.gd
@@ -3,13 +3,13 @@ extends CenterContainer
 @onready var start_game_button = %StartGameButton
 
 func _ready():
-#	RenderingServer.set_default_clear_color(Color.BLACK)
+	RenderingServer.set_default_clear_color(Color.PINK)
 	start_game_button.grab_focus()
 
 func _on_start_game_button_pressed():
-	await LevelTransition.fade_from_black()
+	await LevelTransition.fade_to_black()
 	get_tree().change_scene_to_file("res://Scenes/World.tscn")
-#	LevelTransition.fade_to_black()
+	LevelTransition.fade_from_black()
 
 func _on_quit_button_pressed():
 	get_tree().quit()
