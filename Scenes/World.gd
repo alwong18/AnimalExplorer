3454699d7651abd23a970f55157dd9ee823cf3ba
alwong18 <alwong18@s.sfusd.edu>
2024-03-08 13:40:34 -0800

    Created level completed
    Need to change later to emit when we get key
    
    Video Progress Check 8

---
+++ Scenes/World.gd
@@ -0,0 +1,11 @@
+extends Node
+
+@onready var collision_shape_2d = $StaticBody2D/CollisionShape2D
+@onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
+@onready var level_completed = $CanvasLayer/LevelCompleted
+
+func _ready():
+	Events.level_completed.connect(show_level_completed)
+	
+func show_level_completed():
+	level_completed.show()
