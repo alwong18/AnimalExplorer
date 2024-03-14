c3a01d7e1c336b9438bddb016efd500e7925629c
machen18 <machen18@s.sfusd.edu>
2024-03-14 14:10:29 -0700

    coyote jump timer
    vid 2

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -17,17 +17,21 @@ func _physics_process(delta):
 	handle_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
 	update_animations(input_axis)
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ledge:
+		coyote_jump_timer.start()
 
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
 func handle_jump():
-	if is_on_floor():
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("move_up"):
 			velocity.y = JUMP_VELOCITY
-		else:
+		if not is_on_floor():
 			if Input.is_action_just_released("move_up") and velocity.y < JUMP_VELOCITY / 2:
 				velocity.y = JUMP_VELOCITY / 2
 
