e99a2ef540e1f83927e912d508d1d54a18b18d87
alwong18 <alwong18@s.sfusd.edu>
2024-03-26 11:00:04 -0700

    added level one
    need to finish creating

--- Scripts/start_menu.gd
+++ Scripts/start_menu.gd
@@ -8,7 +8,7 @@ func _ready():
 
 func _on_start_game_button_pressed():
 	await LevelTransition.fade_to_black()
-	get_tree().change_scene_to_file("res://Scenes/World.tscn")
+	get_tree().change_scene_to_file("res://Scenes/level_one.tscn")
 	LevelTransition.fade_from_black()
 
 func _on_quit_button_pressed():
