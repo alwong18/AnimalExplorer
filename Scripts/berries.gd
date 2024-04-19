daff158178c78009cf818f788ddfbf233d1f3cbc
vivianw1 <122054036+vivianw1@users.noreply.github.com>
2024-04-19 14:00:57 -0700

    Changes text

--- Scripts/berries.gd
+++ Scripts/berries.gd
@@ -4,4 +4,4 @@ func _on_body_entered(body):
 	queue_free()
 	var berries = get_tree().get_nodes_in_group("Berries")
 	if berries.size() == 0.25:
-		print("Level Completed")
+		print("Levels Completed")
