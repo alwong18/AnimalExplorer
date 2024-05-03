e40035dd79e435ff06950ecfc09e092fa8c521cb
alwong18 <alwong18@s.sfusd.edu>
2024-05-03 14:28:40 -0700

    added level completed

--- portal.gd
+++ portal.gd
@@ -14,5 +14,5 @@ func _on_area_entered(area):
 	entered = true
 	if area is Player:
 		print("entered portal")
-#		Events.level_completed.emit()
+		Events.level_completed.emit()
 		get_tree().change_scene_to_file("res://Scenes/level_two.tscn")
