4bea0187cff978ac776354442b2be9b18afe6227
alwong18 <alwong18@s.sfusd.edu>
2024-05-03 14:22:49 -0700

    tried to fix portal
    not working

--- portal.gd
+++ portal.gd
@@ -1,6 +1,18 @@
 extends Area2D
 
+var entered = false
 
-func _on_body_entered(body):
-	print("entered portal")
-	Events.level_completed.emit()
+#func _on_body_entered(body):
+#	entered = true
+#	if body is Player:
+#		print("entered portal")
+#		Events.level_completed.emit()
+#		get_tree().change_scene_to_file("res://Scenes/level_two.tscn")
+
+
+func _on_area_entered(area):
+	entered = true
+	if area is Player:
+		print("entered portal")
+#		Events.level_completed.emit()
+		get_tree().change_scene_to_file("res://Scenes/level_two.tscn")
