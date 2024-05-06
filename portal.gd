100f01ad5c2879c058c70a8c486b6f5a60a22d9e
alwong18 <alwong18@s.sfusd.edu>
2024-05-06 13:03:34 -0700

    THE PORTAL WORKS

--- portal.gd
+++ portal.gd
@@ -1,24 +1,17 @@
 extends Area2D
 
+@export var next_level: PackedScene
+
 var entered = false
-
-#func _on_body_entered(body):
-#	entered = true
-#	if body is Player:
-#		print("entered portal")
-#		Events.level_completed.emit()
-#		get_tree().change_scene_to_file("res://Scenes/level_two.tscn")
-
-
-func _on_area_entered(area):
-	entered = true
-	if area is Player:
-		print("entered portal")
-		Events.level_completed.emit()
-		get_tree().change_scene_to_file("res://Scenes/level_two.tscn")
 
 func _process(delta):
 	if entered == true:
 		if Input.is_action_just_pressed("shoot"):
 			get_tree().change_scene_to_file("res://Scenes/level_two.tscn")
-			
+
+func _on_body_entered(body):
+	entered = true
+	if body is Player:
+		print("entered portal")
+		Events.level_completed.emit()
+		get_tree().change_scene_to_file("res://Scenes/level_two.tscn")
