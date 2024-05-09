26e2a9e5efe6041c76af0f8edb5f314d7df512ed
vivianw1 <122054036+vivianw1@users.noreply.github.com>
2024-05-09 13:57:16 -0700

    imma work on lvl completed screen first

--- Scenes/World.gd
+++ Scenes/World.gd
@@ -3,11 +3,15 @@ extends Node
 var level_time = 0.0
 var start_level_msc = 0.0 
 @onready var level_time_label = $CanvasLayer/LevelTimeLabel
+@onready var start_in = $CanvasLayer/StartIn
+@onready var start_in_label = %StartInLabel
+@onready var animation_player = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#get_tree().paused = true
 	start_level_msc = Time.get_ticks_msec()
-	
+	#Events.level_completed.connect(show_level_completed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
