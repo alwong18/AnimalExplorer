d9bda34cdd0e1588d149fa3e0c00bb7a9069ef41
alwong18 <alwong18@s.sfusd.edu>
2024-05-10 13:52:44 -0700

    finished fading transition

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -5,7 +5,6 @@ extends Node
 @onready var level_completed = $CanvasLayer/LevelCompleted
 
 func _ready():
-#	RenderingServer.set_default_clear_color(Color.BLACK)
 	Events.level_completed.connect(show_level_completed)
 	
 func show_level_completed():
