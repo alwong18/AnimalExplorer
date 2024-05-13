2fe439d388138319329b7a7f3594b21133396dc2
alwong18 <alwong18@s.sfusd.edu>
2024-05-13 13:06:52 -0700

    added fading for level to level

--- portal.gd
+++ portal.gd
@@ -14,4 +14,6 @@ func _on_body_entered(body):
 	if body is Player:
 		print("entered portal")
 		Events.level_completed.emit()
+		await LevelTransition.fade_to_black()
 		get_tree().change_scene_to_packed(next_level)
+		LevelTransition.fade_from_black()
