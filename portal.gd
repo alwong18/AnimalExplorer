23e8c8517edca83ae1c09e21b488410333bed286
alwong18 <alwong18@s.sfusd.edu>
2024-05-13 13:44:52 -0700

    added key and enemies

--- portal.gd
+++ portal.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export var next_level: PackedScene
+@onready var key_1 = $"../Key1"
 
 var entered = false
 
@@ -11,9 +12,10 @@ func _process(delta):
 
 func _on_body_entered(body):
 	entered = true
-	if body is Player:
+	if body is Player && key_1.body_entered:
 		print("entered portal")
 		Events.level_completed.emit()
 		await LevelTransition.fade_to_black()
 		get_tree().change_scene_to_packed(next_level)
 		LevelTransition.fade_from_black()
+		
