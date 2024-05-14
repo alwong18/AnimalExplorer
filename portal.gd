6217048ee81d31d1237bc6324c0e9d3f687afd97
alwong18 <alwong18@s.sfusd.edu>
2024-05-14 12:18:41 -0700

    tried key didnt work

--- portal.gd
+++ portal.gd
@@ -5,17 +5,12 @@ extends Area2D
 
 var entered = false
 
-func _process(delta):
-	if entered == true:
-		if Input.is_action_just_pressed("shoot"):
-			get_tree().change_scene_to_file("res://Scenes/level_two.tscn")
-
 func _on_body_entered(body):
 	entered = true
-	if body is Player && key_1.body_entered:
+	if body is Player: #and key_1.body_entered():
 		print("entered portal")
 		Events.level_completed.emit()
 		await LevelTransition.fade_to_black()
 		get_tree().change_scene_to_packed(next_level)
 		LevelTransition.fade_from_black()
-		
+
