94b017efdeabf78cdd37563180f541d364c27085
vivianw1 <122054036+vivianw1@users.noreply.github.com>
2024-05-14 12:28:25 -0700

    commit

--- portal.gd
+++ portal.gd
@@ -4,10 +4,6 @@ extends Area2D
 
 var entered = false
 
-func _process(delta):
-	if entered == true:
-		if Input.is_action_just_pressed("shoot"):
-			get_tree().change_scene_to_file("res://Scenes/level_two.tscn")
 
 func _on_body_entered(body):
 	entered = true
