d64057525cd821949e08881e213e35e8f0870e47
alwong18 <alwong18@s.sfusd.edu>
2024-05-23 14:11:10 -0700

    added boss

--- boss.gd
+++ boss.gd
@@ -1,19 +1,18 @@
 extends CharacterBody2D
 
+
 var direction = Vector2.RIGHT
 
-@onready var ledgeCheckR: =  $LedgeCheckR
-@onready var ledgeCheckL: =  $LedgeCheckL
-@onready var sprite: = $AnimatedSprite2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
 
 func _physics_process(delta):
 	var found_wall = is_on_wall()
-		
+	
 	if found_wall:
 		direction *= -1
 		
-	sprite.flip_h = direction.x > 0
+	animated_sprite_2d.flip_h = direction.x > 0
 	
 	velocity = direction * 25
 	move_and_slide()
-	
